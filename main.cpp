dce5f30b60ee77291e11c72b734846ba72b95c46
WAGTTZ <wagttz@yahoo.com>
2026-04-20 18:01:39 +0800

    ft: bar

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 void foo() {}
-void bar() {}
+void bar() {
+    cout << "It’s bar!!" << endl; // 修改這行
+}
+
 int main() {
     cout << "Hello, World!" << endl;
+    bar(); // 在這裡呼叫 bar
     return 0;
 }
